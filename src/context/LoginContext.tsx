a5f47e7ad09d0441ec0948f1b6cab58d28af67f7
techstackmedia1 <osagienoah04@gmail.com>
2023-03-06 12:34:34 -0800

    build: (LoginContext.tsx) create login context file

---
+++ src/context/LoginContext.tsx
@@ -0,0 +1,210 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/react-in-jsx-scope */
+import React, { useState, createContext, ChangeEvent, FormEvent } from 'react'
+
+import { LoginContextInterface } from './LoginContextInterface'
+
+const LoginContext = createContext<LoginContextInterface>({
+  email: '',
+  password: '',
+  error: '',
+  err: '',
+  showTextPassword: false,
+  textPasswordType: 'password',
+  loggedIn: undefined,
+  validationMessage: '',
+  handleEmailChange: () => {},
+  handlePasswordChange: () => {},
+  handleSubmit: async () => {},
+  handleLogoutClick: () => {},
+  showPassword: () => {},
+  eyeWatchIcon: <></>,
+  dayHr: '',
+  day: '',
+  dateNum: '',
+  month: '',
+  pmAM: '',
+  timer: '',
+})
+
+const LoginProvider = ({
+  children,
+}: {
+  children: React.ReactNode
+}): JSX.Element => {
+  // web app login
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState<string>('')
+  const [error, setError] = useState<string>('')
+  const [showTextPassword, setShowTextPassword] = useState<boolean>(false)
+  const [textPasswordType, setTextPasswordType] = useState<string>('password')
+  const [loggedIn, setLoggedIn] = useState<boolean | undefined>(false)
+  const [validationMessage, setValidationMessage] = useState<string>('')
+  const [err, setErr] = useState('')
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setEmail(e.target.value)
+  }
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setPassword(e.target.value)
+  }
+
+  const showPassword = (): void => {
+    if (!showTextPassword) {
+      setShowTextPassword(true)
+      setTextPasswordType('text')
+    } else {
+      setShowTextPassword(false)
+      setTextPasswordType('password')
+    }
+  }
+
+  // testing purpose
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault()
+
+    try {
+      const credential = {
+        email,
+        password,
+      }
+
+      if (
+        (document.querySelectorAll('input')[0].value === '' &&
+          email.trim().length === 0) ||
+        (document.querySelectorAll('input')[1].value === '' &&
+          password.trim().length === 0) ||
+        // !validator.isEmail(email)
+        (!email.includes('@') && !email.includes('.'))
+      ) {
+        if (document.querySelectorAll('input')[0].value === '') {
+          setValidationMessage('Email is required')
+        } else if (document.querySelectorAll('input')[1].value === '') {
+          setValidationMessage('Password is required')
+        }
+      }
+
+      const response = await fetch(`/api/v2/auth/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credential),
+      })
+
+      if (response.status === 400) {
+        // throw new Error('Invalid username or password')
+        // const { statusText } = response
+        // setErr(`${statusText}. Try again`)
+        setErr('Bad Request. Try again')
+      }
+
+      if (!response.ok) {
+        throw new Error('Authentication failed')
+      }
+
+      const data = await response.json()
+      localStorage.setItem('token', data.token)
+
+      if (data.token === undefined) {
+        setLoggedIn(undefined)
+      } else {
+        setLoggedIn(true)
+      }
+    } catch (event) {
+      setError('Email or Password is Incorrect')
+    }
+  }
+
+  const handleLogoutClick = (): void => {
+    localStorage.removeItem('token')
+    setLoggedIn(false)
+    window.location.assign('/login')
+  }
+
+  const eyeWatchIcon = (
+    <div onClick={showPassword}>
+      {showTextPassword ? (
+        <img
+          src="https://res.cloudinary.com/bizstak/image/upload/v1678128269/opened-eye_cfytw0.svg"
+          width={24}
+          height={24}
+          alt="eye opened"
+        />
+      ) : (
+        <img
+          src="https://res.cloudinary.com/bizstak/image/upload/v1678128224/closed-eye_giryen.svg"
+          width={24}
+          height={24}
+          alt="eye closed"
+        />
+      )}
+    </div>
+  )
+
+  // date
+  const date = new Date()
+  const datetoString = date.toString()
+  const datetoArr = datetoString.split(' ')
+  datetoArr.splice(5)
+  const time = datetoArr[datetoArr.length - 1]
+  const timetoArr = time.split(':')
+  const hr = +timetoArr[0]
+
+  let dayHr: undefined | string
+  if (hr > 0 && hr < 12) {
+    dayHr = 'Morning'
+  } else if (hr >= 12 && hr < 17) {
+    dayHr = 'Afternoon'
+  } else if (hr >= 17 && hr <= 20) {
+    dayHr = 'Evening'
+  } else {
+    dayHr = 'Night'
+  }
+
+  const dateString: string = date.toDateString()
+  const dateArr: string[] = dateString.split(' ')
+  const dateLocale = date.toLocaleString()
+  const day: string = dateArr[0]
+  const dateNum: string = dateArr[2]
+  const month: string = dateArr[1]
+  const dateLocaleArr = dateLocale.split(' ')
+  const timeLocale = dateLocaleArr[1]
+  const pmAM = dateLocale.split(' ')[2]
+  const timeLocaleArr = timeLocale.split(':')
+  timeLocaleArr.splice(2)
+  timeLocaleArr.join('')
+  const timer = timeLocaleArr.join(':')
+
+  return (
+    <LoginContext.Provider
+      value={{
+        email,
+        password,
+        error,
+        err,
+        showTextPassword,
+        textPasswordType,
+        loggedIn,
+        validationMessage,
+        handleEmailChange,
+        handlePasswordChange,
+        handleSubmit,
+        handleLogoutClick,
+        showPassword,
+        eyeWatchIcon,
+        dayHr,
+        day,
+        dateNum,
+        month,
+        pmAM,
+        timer,
+      }}>
+      {children}
+    </LoginContext.Provider>
+  )
+}
+
+export default LoginContext
+export { LoginProvider }
