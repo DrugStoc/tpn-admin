506348f2e2fe78d172ae0da65d5a3ed0062897b9
techstackmedia1 <osagienoah04@gmail.com>
2023-03-06 12:23:40 -0800

    build: (ButtonInterface.ts) create custom button type defining interface

---
+++ src/components/shared/Button/ButtonInterface.ts
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+interface buttonInterface {
+  text: string | undefined
+  textColor: string
+  bgColor: string
+  buttonWidth: string
+  buttonBorderRadius: string
+  buttonBorder: string
+  fontSize: string
+  handleButtonClick?: VoidFunction
+  buttonHeight: string
+  fontWeight: string
+  fontFamily: string
+  linkText: string | undefined | null
+  type: 'button' | 'reset' | 'submit' | undefined
+}
+
+export type { buttonInterface }
