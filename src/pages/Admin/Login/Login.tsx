38c5bf228b4094d90a4e4c77af142cc04d9a4dfc
techstackmedia1 <osagienoah04@gmail.com>
2023-03-06 12:37:43 -0800

    build: (Login.tsx) create login page

---
+++ src/pages/Admin/Login/Login.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/react-in-jsx-scope */
+import Login from '../../../components/Admin/Login/Login'
+
+const LoginAdmin = (): JSX.Element => {
+  return (
+    <div>
+      <LoginAdmin />
+    </div>
+  )
+}
+
+export default Login
