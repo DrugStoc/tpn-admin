df0d1bf5a4e779528b0db10b28966be593f11c43
techstackmedia1 <osagienoah04@gmail.com>
2023-03-07 15:27:05 -0800

    chore: (App.ts) remove comments and select end of line sequence

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/react-in-jsx-scope */
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Admin/Login/Login'
 
