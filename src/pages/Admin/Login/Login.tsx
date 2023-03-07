e7aaf60f94c3e46b631dc25f7fe251d002e0bb1b
techstackmedia1 <osagienoah04@gmail.com>
2023-03-07 15:33:00 -0800

    chore: (Login.tsx) remove comments and select end of line sequence

--- src/pages/Admin/Login/Login.tsx
+++ src/pages/Admin/Login/Login.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable react/react-in-jsx-scope */
+import React from 'react'
 import Login from '../../../components/Admin/Login/Login'
 
 const LoginAdmin = (): JSX.Element => {
