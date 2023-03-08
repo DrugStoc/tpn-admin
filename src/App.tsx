3d66659255b682aa1d66a64ac970fe2f4835716b
techstackmedia1 <osagienoah04@gmail.com>
2023-03-08 09:48:15 -0800

    feat: (App.tsx) provide login context provider to the entire app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Admin/Login/Login'
+import Login from './pages/Login'
+import { LoginProvider } from './context/LoginContext'
 
 function App(): JSX.Element {
   return (
-    <>
+    <LoginProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </LoginProvider>
   )
 }
 
