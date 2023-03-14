ce73807d53437ce9689c7793319d5d2a413558cd
techstackmedia1 <osagienoah04@gmail.com>
2023-03-14 02:18:26 -0700

    style: add customer styles and its responsiveness

--- src/config/addCustomer.ts
+++ src/config/addCustomer.ts
@@ -9,38 +9,36 @@ const addCustomer = {
     w: 'calc(100% - 200px)',
     ml: 'auto',
   },
-  '.addCustomer .addCustomer-card': { mb: '50px' },
+  '.addCustomer .card': { mb: '50px' },
   '.addCustomer .addCustomer-section': { w: '90%' },
-  '.addCustomer .addCustomer-section .addCustomer-cardBody': {
+  '.addCustomer .addCustomer-section .cardBody': {
     py: '40px',
     px: '55px',
   },
-  '.addCustomer .addCustomer-section .addCustomer-cardBody h1': {
+  '.addCustomer .addCustomer-section .cardBody h1': {
     fontSize: '24px',
   },
-  '.addCustomer .addCustomer-section .addCustomer-cardBody .buttonHeading': {
+  '.addCustomer .addCustomer-section .cardBody .buttonHeading': {
     display: 'flex',
     justifyContent: 'space-between',
     mt: '45px',
   },
-  '.addCustomer .addCustomer-section .addCustomer-cardBody .buttonHeading button':
-    {
-      color: '#514f6d',
-      fontSize: '14px',
-    },
-  '.addCustomer .addCustomer-section .addCustomer-cardBody .buttonHeading .buttonImage':
-    {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      gap: '8px',
-      cursor: 'pointer',
-    },
-  '.addCustomer .addCustomer-section .addCustomer-cardBody form,\n.addCustomer .addCustomer-section .addCustomer-cardBody .deliveryForm':
+  '.addCustomer .addCustomer-section .cardBody .buttonHeading button': {
+    color: '#514f6d',
+    fontSize: '14px',
+  },
+  '.addCustomer .addCustomer-section .cardBody .buttonHeading .buttonImage': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    gap: '8px',
+    cursor: 'pointer',
+  },
+  '.addCustomer .addCustomer-section .cardBody form,\n.addCustomer .addCustomer-section .cardBody .deliveryForm':
     {
       mt: '60px',
     },
-  '.addCustomer .addCustomer-section .addCustomer-cardBody form .form,\n.addCustomer .addCustomer-section .addCustomer-cardBody .deliveryForm .form':
+  '.addCustomer .addCustomer-section .cardBody form .form,\n.addCustomer .addCustomer-section .cardBody .deliveryForm .form':
     {
       display: 'flex',
       justifyContent: 'space-between',
@@ -48,19 +46,19 @@ const addCustomer = {
       mt: '-20px',
       mb: '-20px',
     },
-  '.addCustomer .addCustomer-section .addCustomer-cardBody form :where(.inputLabel),\n.addCustomer .addCustomer-section .addCustomer-cardBody .deliveryForm :where(.inputLabel)':
+  '.addCustomer .addCustomer-section .cardBody form :where(.inputLabel),\n.addCustomer .addCustomer-section .cardBody .deliveryForm :where(.inputLabel)':
     {
       mb: '30px',
       minW: '405.922px',
       pos: 'relative',
     },
-  '.addCustomer .addCustomer-section .addCustomer-cardBody form :where(.inputLabel) :where(img),\n.addCustomer .addCustomer-section .addCustomer-cardBody .deliveryForm :where(.inputLabel) :where(img)':
+  '.addCustomer .addCustomer-section .cardBody form :where(.inputLabel) :where(img),\n.addCustomer .addCustomer-section .cardBody .deliveryForm :where(.inputLabel) :where(img)':
     {
       pos: 'absolute',
       top: '53px',
-      left: '22px',
+      right: '17px',
     },
-  '.addCustomer .addCustomer-section .addCustomer-cardBody form :where(.inputLabel) label,\n.addCustomer .addCustomer-section .addCustomer-cardBody .deliveryForm :where(.inputLabel) label':
+  '.addCustomer .addCustomer-section .cardBody form :where(.inputLabel) label,\n.addCustomer .addCustomer-section .cardBody .deliveryForm :where(.inputLabel) label':
     {
       fontSize: '14px',
       color: '#1f1c19',
@@ -72,7 +70,7 @@ const addCustomer = {
     border: '1px solid #f9f9fc',
     mt: '20px',
     h: '30px',
-    textIndent: '40px',
+    // textIndent: '40px',
     w: '100%',
     outline: 'none',
     p: '20px',
@@ -88,6 +86,7 @@ const addCustomer = {
     borderRadius: '4px',
   },
   '.addCustomer .inputLabel': { pos: 'relative' },
+  '.addMerchant .inputLabel .inputDelivery': { textIndent: '3px !important' },
   '.addCustomer .inputLabel img.arrow': {
     pos: 'absolute !important',
     right: '15px !important',
