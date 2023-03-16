0e80feccd3686d0c393f971c5de7a76c5c98e1f1
techstackmedia1 <osagienoah04@gmail.com>
2023-03-16 01:15:34 -0700

    build: (SidebarData.ts) create sidebar data

---
+++ src/components/Sidebar/SidebarData.ts
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+const sidebarTopList: string[] = [
+  'Overview',
+  'Customers',
+  'Orders',
+  'Merchants',
+  'Products',
+  'Shippings',
+]
+
+const sidebarBottomList: string[] = ['Settings', 'Logout']
+
+export { sidebarTopList, sidebarBottomList }
