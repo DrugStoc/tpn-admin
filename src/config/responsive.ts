a75eb62c6533021b5c3d2266585869060fff6a63
techstackmedia1 <osagienoah04@gmail.com>
2023-03-16 02:12:11 -0700

    style: add sidebar responsive styles (responsive.ts)

--- src/config/responsive.ts
+++ src/config/responsive.ts
@@ -1,4 +1,29 @@
 const responsive = {
+  '@media (max-width: 700px)': {
+    '.sidebar': {
+      py: 7.5,
+      justifyContent: 'center',
+      alignItems: 'center',
+      w: 'auto',
+    },
+    '.sidebar a': { display: 'none' },
+    '.sidebar li': {
+      w: 'auto',
+      p: '0 10px 0 10px',
+      ml: 'initial',
+    },
+    '.sidebar li img': {
+      mr: 'initial',
+      pos: 'relative',
+      zIndex: 4,
+    },
+    '.sidebar span': { display: 'none' },
+    '.settingLogout': { mt: 'auto', ml: 'initial !important' },
+    '.sidebar-box': { mx: 'auto !important' },
+  },
+  '@media (max-width: 700px) and (min-width: 557px)': {
+    '.sidebar': { ml: 1 },
+  },
   '@media (max-width: 679px)': {
     '.login-card': {
       w: '90%',
@@ -6,7 +31,7 @@ const responsive = {
   },
   '@media (max-width: 575px)': {
     '.login-card': {
-      w: '100%',
+      w: 'full',
       borderRadius: 0,
       boxShadow: 'none',
       p: '0 10px',
