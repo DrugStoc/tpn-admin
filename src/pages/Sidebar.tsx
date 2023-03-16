fe2bf366d9884c2619d39231b34f10cabd8c36e1
techstackmedia1 <osagienoah04@gmail.com>
2023-03-16 02:34:05 -0700

    build: create a page that contain sidebar component (sidebar.tsx)

---
+++ src/pages/Sidebar.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/react-in-jsx-scope */
+import SidebarAside from '../components/Sidebar/Sidebar'
+
+const Sidebar = (): JSX.Element => {
+  return (
+    <>
+      <SidebarAside />
+      <p>Hello world</p>
+    </>
+  )
+}
+
+export default Sidebar
