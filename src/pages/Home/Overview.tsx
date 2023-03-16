46a7204dfbefa5b62281a9273f3559108729d6d4
techstackmedia1 <osagienoah04@gmail.com>
2023-03-16 13:17:40 -0700

    build: create home or overview page (Overview.tsx)

---
+++ src/pages/Home/Overview.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/react-in-jsx-scope */
+import Overview from '../../components/Overview/Overview'
+import SidebarAside from '../../components/Sidebar/Sidebar'
+
+const Home = (): JSX.Element => {
+  return (
+    <>
+      <SidebarAside />
+      <Overview />
+    </>
+  )
+}
+
+export default Home
