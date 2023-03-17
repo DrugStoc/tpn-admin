f13a6baaf3ec59c4b304e9cac7ce7982e126a437
techstackmedia1 <osagienoah04@gmail.com>
2023-03-17 05:02:41 -0700

    style: change dropdown filter paragraph color (filter.ts)

--- src/config/filter.ts
+++ src/config/filter.ts
@@ -46,7 +46,7 @@ const filter = {
               'rgba(0, 0, 0, 0.2) 0px 5px 5px -3px, rgba(0, 0, 0, 0.14) 0px 8px 10px 1px, rgba(0, 0, 0, 0.12) 0px 3px 14px 2px',
             ul: {
               p: {
-                color: 'brand.450',
+                color: 'brand.350',
                 p: '10px 15px',
               },
               li: {
