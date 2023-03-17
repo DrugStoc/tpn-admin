d6f4521a3324810ad04dd322b7beb3f6d915e9b5
techstackmedia1 <osagienoah04@gmail.com>
2023-03-17 02:48:29 -0700

    feat: create responsive style for input component (responsive.ts)

--- src/config/responsive.ts
+++ src/config/responsive.ts
@@ -1,4 +1,31 @@
 const responsive = {
+  '@media (max-width: 1088px)': {
+    '.inputButton': {
+      flexWrap: 'wrap',
+      '.dropdown': {
+        left: 'initial !important',
+        right: '0',
+      },
+    },
+    '.inputSearch': {
+      w: '100% !important',
+    },
+  },
+  '@media (max-width: 963px)': {
+    '.inputButton': {
+      flexWrap: 'wrap',
+      button: {
+        w: '40% !important',
+      },
+    },
+  },
+  '@media (max-width: 785px)': {
+    '.inputButton': {
+      button: {
+        w: '50% !important',
+      },
+    },
+  },
   '@media (max-width: 700px)': {
     '.sidebar': {
       py: '30px',
@@ -33,6 +60,20 @@ const responsive = {
       p: '10px',
     },
   },
+  '@media (max-width: 675px)': {
+    '.inputButton': {
+      button: {
+        w: '60% !important',
+      },
+    },
+  },
+  '@media (max-width: 600px)': {
+    '.inputButton': {
+      button: {
+        w: '80% !important',
+      },
+    },
+  },
   '@media (max-width: 575px)': {
     '.login-card': {
       w: 'full',
@@ -48,6 +89,25 @@ const responsive = {
     '.navbar .clock': {
       pos: 'relative',
       top: 0,
+    },
+  },
+  '@media (max-width: 525px)': {
+    '.inputButton': {
+      top: '-12px !important',
+      button: {
+        mt: '13px',
+      },
+    },
+    '.navbar .clock': {
+      position: 'relative',
+      top: '0',
+    },
+  },
+  '@media (max-width: 505px)': {
+    '.inputButton': {
+      button: {
+        w: '100% !important',
+      },
     },
   },
   '@media (max-width: 480px)': {
