cdc25bf3736e332b634dea46ea3ade7c873b1b99
techstackmedia1 <osagienoah04@gmail.com>
2023-03-17 04:57:10 -0700

    feat: add summary component to overview component (Overview.tsx)

--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Motion from '../shared/Motion'
 import Navbar from '../Navbar/Navbar'
 import Filter from '../Filter/Filter'
+import Summary from '../Summary/Summary'
 
 const Overview = (): JSX.Element => {
   return (
@@ -10,6 +11,10 @@ const Overview = (): JSX.Element => {
       <div className="section">
         <Navbar arrow={''} nav={undefined} />
         <Filter />
+        <section className="fillSearch">
+          <Summary />
+          <div></div>
+        </section>
       </div>
     </Motion>
   )
