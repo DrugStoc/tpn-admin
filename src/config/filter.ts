0a00d1932408efa00e6d2fe8a06e120c486b9002
techstackmedia1 <osagienoah04@gmail.com>
2023-03-19 14:57:19 -0700

    style: recheck and correct dropdown box shadow invalid property value(filter.ts)

--- src/config/filter.ts
+++ src/config/filter.ts
@@ -43,7 +43,7 @@ const filter = {
             w: '12.5rem',
             zIndex: '2',
             boxShadow:
-              'rgba(0, 0, 0, 0.2) 0 0.3125rem 0.3125rem -0.1875rem, rgba(0, 0, 0, 0.14) 0 0.5rem 0.625rem 0.0625, rgba(0, 0, 0, 0.12) 0 0.1875rem 0.875rem 0.125rem',
+              '0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12)',
             ul: {
               p: {
                 color: 'brand.350',
