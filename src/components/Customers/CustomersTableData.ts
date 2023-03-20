9efd82d6de1d96cfedb6d997a602df7a251c0aea
techstackmedia1 <osagienoah04@gmail.com>
2023-03-20 10:31:46 -0700

    build: create data for customers component (CustomersTableData.ts)

---
+++ src/components/Customers/CustomersTableData.ts
@@ -0,0 +1,119 @@
+const CustomersTableData = [
+  {
+    id: 1,
+    row: [
+      { 'column 2': 'Matthew' },
+      { 'column 3': 'Obichukwu' },
+      { 'column 4': 'example@gmail.com' },
+      { 'column 5': '08143577878' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+      { 'column 7': 0 },
+      { 'column 8': 0 },
+      { 'column 9': 0 },
+    ],
+  },
+  {
+    id: 2,
+    row: [
+      { 'column 2': 'Jack' },
+      { 'column 3': 'Phil' },
+      { 'column 4': 'jackphil@gmail.com' },
+      { 'column 5': '08148493028' },
+      { 'column 6': 'Thurs 6 Oct, 12:18' },
+      { 'column 7': 0 },
+      { 'column 8': 0 },
+      { 'column 9': 0 },
+    ],
+  },
+  {
+    id: 3,
+    row: [
+      { 'column 2': 'Michael' },
+      { 'column 3': 'Owen' },
+      { 'column 4': 'michaelowen@gmail.com' },
+      { 'column 5': '0814279878' },
+      { 'column 6': 'Wed 5 Oct, 11:20' },
+      { 'column 7': 0 },
+      { 'column 8': 0 },
+      { 'column 9': 0 },
+    ],
+  },
+  {
+    id: 4,
+    row: [
+      { 'column 2': 'Bob' },
+      { 'column 3': 'Jesse' },
+      { 'column 4': 'bobjesse@gmail.com' },
+      { 'column 5': '08143877778' },
+      { 'column 6': 'Tues 6 Oct, 4:30' },
+      { 'column 7': 0 },
+      { 'column 8': 0 },
+      { 'column 9': 0 },
+    ],
+  },
+  {
+    id: 5,
+    row: [
+      { 'column 2': 'Bello' },
+      { 'column 3': 'Osagie' },
+      { 'column 4': 'belloosagie@gmail.com' },
+      { 'column 5': '08143577878' },
+      { 'column 6': 'Mon 5 Oct, 3:28' },
+      { 'column 7': 0 },
+      { 'column 8': 0 },
+      { 'column 9': 0 },
+    ],
+  },
+  {
+    id: 6,
+    row: [
+      { 'column 2': 'Monica' },
+      { 'column 3': 'Elizebeth' },
+      { 'column 4': 'monicaelizebeth@gmail.com' },
+      { 'column 5': '08143577878' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+      { 'column 7': 0 },
+      { 'column 8': 0 },
+      { 'column 9': 0 },
+    ],
+  },
+  {
+    id: 7,
+    row: [
+      { 'column 2': 'Felix' },
+      { 'column 3': 'Smart' },
+      { 'column 4': 'felixsmart@gmail.com' },
+      { 'column 5': '0814885828' },
+      { 'column 6': 'Sun 4 Oct, 1:12' },
+      { 'column 7': 0 },
+      { 'column 8': 0 },
+      { 'column 9': 0 },
+    ],
+  },
+  {
+    id: 8,
+    row: [
+      { 'column 2': 'Timothy' },
+      { 'column 3': 'Smulga' },
+      { 'column 4': 'timothysmulga@gmail.com' },
+      { 'column 5': '08043577878' },
+      { 'column 6': 'Sat 3 Oct, 9:03' },
+      { 'column 7': 0 },
+      { 'column 8': 0 },
+      { 'column 9': 0 },
+    ],
+  },
+]
+
+const CustomersTableHeadingData = [
+  'empty',
+  'First Name',
+  'Last Name',
+  'Email',
+  'Phone Number',
+  'Joined',
+  'Voucher Redeemed',
+  'Requested Products',
+]
+
+export { CustomersTableData, CustomersTableHeadingData }
