39d50c0ff328a39a72edfa9d024561ebab131bab
techstackmedia1 <osagienoah04@gmail.com>
2023-03-21 05:04:28 -0700

    refactor: remove unnecessary code and import relevant module(s) (sidebar.ts)

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -1,20 +1,19 @@
+import { dCenter, dVerticalBtw } from "./general"
+
 const sidebar = {
   '.sidebar': {
     pos: 'fixed',
     w: '12.5rem',
     h: '100vh',
     bgColor: 'brand.600',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
+    ...dVerticalBtw,
     fontFamily: 'fontFamily',
     alignItems: 'stretch',
     overflowY: 'auto',
     color: 'brand.100',
     zIndex: 1,
     li: {
-      display: 'flex',
-      alignItems: 'center',
+      ...dCenter,
       p: '0 2.5rem 0 1.875rem',
       ml: '-0.625rem',
       h: '2.5rem',
