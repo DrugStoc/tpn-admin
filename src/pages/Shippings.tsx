b20906730a546f8f82c8c84d7b69f8fe0640bfa7
techstackmedia1 <osagienoah04@gmail.com>
2023-03-21 06:17:47 -0700

    build: create shippings page on app (Shippings.tsx)

---
+++ src/pages/Shippings.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ShippingsAside from '../components/Shippings/Shippings'
+import SidebarAside from '../components/Sidebar/Sidebar'
+
+const Shippings = (): JSX.Element => {
+  return (
+    <>
+      <SidebarAside />
+      <ShippingsAside />
+    </>
+  )
+}
+
+export default Shippings
