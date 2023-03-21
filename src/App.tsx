f9680baebea6ad12af66c77efafe719f1a98acee
techstackmedia1 <osagienoah04@gmail.com>
2023-03-21 06:18:44 -0700

    feat: route to shippings page (Shippings.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Orders from './pages/Orders'
 import Customers from './pages/Customers'
 import Merchants from './pages/Merchants'
 import Products from './pages/Products'
+import Shippings from './pages/Shippings'
 
 function App(): JSX.Element {
   return (
@@ -20,6 +21,7 @@ function App(): JSX.Element {
             <Route path="/customers" element={<Customers />} />
             <Route path="/merchants" element={<Merchants />} />
             <Route path="/products" element={<Products />} />
+            <Route path="/shippings" element={<Shippings />} />
           </Routes>
         </main>
       </BrowserRouter>
