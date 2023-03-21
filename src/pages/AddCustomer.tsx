54fcc22041f5b3183d8d7da75470dc93b4b373e8
techstackmedia1 <osagienoah04@gmail.com>
2023-03-21 15:58:21 -0700

    build: create add customer page to app (AddCustomer.tsx)

---
+++ src/pages/AddCustomer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SidebarAside from '../components/Sidebar/Sidebar'
+import AddCustomerView from '../components/AddCustomer/AddCustomer'
+
+const AddCustomer = (): JSX.Element => {
+  return (
+    <>
+      <SidebarAside />
+      <AddCustomerView />
+    </>
+  )
+}
+
+export default AddCustomer
