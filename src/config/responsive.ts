90bf94912dfff8e52d0f28d01a7f40c3b6f4d44a
techstackmedia1 <osagienoah04@gmail.com>
2023-03-22 06:55:39 -0700

    style: make add shipping page/component responsive (responsive.ts)

--- src/config/responsive.ts
+++ src/config/responsive.ts
@@ -98,7 +98,7 @@ const responsive = {
     '.sidebar span': { display: 'none' },
     '.settingLogout': { mt: 'auto', ml: 'initial !important' },
     '.sidebar-box': { mx: 'auto !important' },
-    '.mainSection, .addCustomer, .addMerchant, .addProduct': {
+    '.mainSection, .addCustomer, .addMerchant, .addProduct, .addShipping': {
       w: 'auto',
       ml: '46px',
     },
