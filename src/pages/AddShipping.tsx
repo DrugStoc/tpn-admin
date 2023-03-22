ae1c8dd05b4ce776f9af386cb3855a94dbc4cd3d
techstackmedia1 <osagienoah04@gmail.com>
2023-03-22 07:00:13 -0700

    build: create add shipping component/page component (AddShipping.tsx)

---
+++ src/pages/AddShipping.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SidebarAside from '../components/Sidebar/Sidebar'
+import AddShippingView from '../components/AddShipping/AddShipping'
+
+const AddShipping = (): JSX.Element => {
+  return (
+    <>
+      <SidebarAside />
+      <AddShippingView />
+    </>
+  )
+}
+
+export default AddShipping
