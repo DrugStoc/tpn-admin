ea4adec84c52eb38e2d600bbe79fed362934d12f
techstackmedia1 <osagienoah04@gmail.com>
2023-03-22 06:23:31 -0700

    build: create add product page/component (AddProduct.tsx)

---
+++ src/pages/AddProduct.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SidebarAside from '../components/Sidebar/Sidebar'
+import AddProductView from '../components/AddProduct/AddProduct'
+
+const AddProduct = (): JSX.Element => {
+  return (
+    <>
+      <SidebarAside />
+      <AddProductView />
+    </>
+  )
+}
+
+export default AddProduct
