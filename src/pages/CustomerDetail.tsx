5f00ac4bac0071e3d7fd4afa3d3dd223ff3e2008
techstackmedia1 <osagienoah04@gmail.com>
2023-03-22 16:47:24 -0700

    build: create customer detail page (CustomerDetails.tsx)

---
+++ src/pages/CustomerDetail.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SidebarAside from '../components/Sidebar/Sidebar'
+import CustomerDetailsView from '../components/CustomerDetails/CustomerDetails'
+
+const CustomerDetails = (): JSX.Element => {
+  return (
+    <>
+      <SidebarAside />
+      <CustomerDetailsView />
+    </>
+  )
+}
+
+export default CustomerDetails
