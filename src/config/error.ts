d0cc8afd960a53112dbae3abdba41c2607d073e1
techstackmedia1 <osagienoah04@gmail.com>
2023-03-23 09:24:23 -0700

    style: style error component/page (error.ts)

---
+++ src/config/error.ts
@@ -0,0 +1,20 @@
+const error = {
+  '.error': {
+    textAlign: 'center',
+    w: '90%',
+    fontWeight: 600,
+    img: {
+      height: '60vh',
+    },
+    h2: {
+      fontSize: '1.2rem',
+      my: 4,
+      span: {
+        cursor: 'pointer',
+        color: 'brand.450',
+      },
+    },
+  },
+}
+
+export default error
