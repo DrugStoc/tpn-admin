d3894dc069ddab5c1f91bf4cc57651a20c0b9948
techstackmedia1 <osagienoah04@gmail.com>
2023-03-26 10:26:07 -0700

    refactor: refactor order detail component (OrderDetails.tsx)

--- src/components/OrderDetails/OrderDetails.tsx
+++ src/components/OrderDetails/OrderDetails.tsx
@@ -1,76 +1,71 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
-import Card from '../shared/Card'
-import Motion from '../shared/Motion'
 import { useParams } from 'react-router-dom'
+import Navbar from '../Navbar/Navbar'
+import Motion from '../shared/Motion'
+import OrderIdentity from './OrderIdentity'
+import OrderList from './OrderList'
 import { OrdersTableData } from '../Orders/OrdersTableData'
 
-const OrdersDetail = ({ arrow }: any): JSX.Element => {
+const OrderDetails = ({ arrow }: any): JSX.Element => {
   const { id }: any = useParams()
-  console.log(id)
   const findId: any = OrdersTableData.find((item: any) => {
     return item.id === +id
   })
+
+  const pharmacist = findId.row[4]['column 6']
+  const orderId = findId.row[0]['column 2']
+  const orderQty = findId.row[5]['column 7']
+  const customerName = findId.row[1]['column 3']
+  const customerPhoneNumber = findId.row[3]['column 5']
+  const customerEmail = findId.row[2]['column 4']
+  const purchaseTime = findId.row[6]['column 8']
+  const timer = purchaseTime.split(',')
+  let time: string = timer[timer.length - 1]
+  const splitTime = time.split(':')
+  const converter = +splitTime[0].trim()
+  const converterRest = +splitTime[1].trim()
+
+  if (converter > 12 && converter < 24) {
+    if (`${converter - 12}`.length > 1) {
+      time = `${converter - 12}:${converterRest} PM`
+    } else {
+      time = `0${converter - 12}:${converterRest} PM`
+    }
+  } else {
+    if (`${converter - 12}`.length > 1) {
+      time = `${time} AM`
+    } else {
+      time = `0${time} AM`
+    }
+  }
 
   return (
     <Motion>
       <div className="addCustomer">
         <Navbar nav="Orders" arrow={arrow} text="Order Details" />
         <section className="addCustomer-section">
-          <Card className="card">
-            <div className="cardBody">
-              <div>
-                <div className="orderDetails">
-                  <div className="ownerHeader">
-                    <h1>Product List</h1>
-                    <h1>
-                      <span>Order via:</span> {findId.row[1]['column 3']}
-                    </h1>
-                  </div>
-                </div>
-                <div className="ownerIdentity">
-                  <div className="ownerId">{findId.row[0]['column 2']}</div>
-                  <div className="ownerId">
-                    No. of products: <span>{findId.row[5]['column 7']}</span>
-                  </div>
-                </div>
-                <div className="liner"></div>
-              </div>
-              <div className="overflow">
-                <div className="row">
-                  {[...Array(findId.row[5]['column 7'])].map((_, index) => {
-                    return (
-                      <div className="rowBtw" key={index}>
-                        <div className="rowFirst">
-                          <img src="https://res.cloudinary.com/bizstak/image/upload/v1679618063/panadol_mwgzos.png" />
-                          <div className="productDetail">
-                            <h3>Panadol</h3>
-                            <h4 className="productDescription">
-                              Acephen, Mapap, Q-Pap, and Tylenol Used for Pain
-                              and Fever
-                            </h4>
-                            <h4 className="productComposition">
-                              325mg Acetaminophen (30 tablets)
-                            </h4>
-                          </div>
-                        </div>
-                        <div className="rowPrice">NGN 700</div>
-                      </div>
-                    )
-                  })}
-                </div>
-              </div>
-              <div className="liner"></div>
-              <div className="rowAmount">
-                <div className="amount">Total Amount:</div>
-                <div className="sum">NGN {700 * findId.row[5]['column 7']}</div>
-              </div>
-            </div>
-          </Card>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              flexWrap: 'wrap',
+            }}>
+            <OrderList
+              pharmacist={pharmacist}
+              orderId={orderId}
+              orderQty={orderQty}
+            />
+            <OrderIdentity
+              customerName={customerName}
+              customerPhoneNumber={customerPhoneNumber}
+              customerEmail={customerEmail}
+              time={time}
+            />
+          </div>
         </section>
       </div>
     </Motion>
   )
 }
 
-export default OrdersDetail
+export default OrderDetails
