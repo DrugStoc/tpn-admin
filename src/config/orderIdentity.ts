6ce916921464d70be658f2b077ccb6c001ddd10a
techstackmedia1 <osagienoah04@gmail.com>
2023-03-26 10:55:38 -0700

    style: include order identity component styles

---
+++ src/config/orderIdentity.ts
@@ -0,0 +1,51 @@
+const orderIdentity = {
+  '.orderIdentity': {
+    w: '34%',
+    '.cardP': {
+      padding: '20px 20px 5px 20px',
+      '.general': {
+        display: 'flex',
+        justifyContent: 'space-between',
+        h2: {
+          fontSize: 14,
+        },
+        h3: {
+          fontSize: 12,
+        },
+      },
+      '.liner': {
+        borderBottom: '0.0625rem solid #E6E1E5',
+        marginTop: '10px',
+      },
+      '.verticalAlign': {
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexDirection: 'column',
+        marginBlock: '10px',
+        '.row': {
+          display: 'flex',
+          justifyContent: 'space-between',
+          flexWrap: 'wrap',
+          marginBlock: '10px',
+          '.rowFirst': {
+            fontSize: 12,
+            color: '#787579',
+          },
+          '.rowSecondLiner': {
+            fontSize: 14,
+            color: '#556AB0',
+            textDecoration: 'underline',
+            fontWeight: 500,
+          },
+          '.rowSecond': {
+            fontSize: 14,
+            color: '#484649',
+            fontWeight: 500,
+          },
+        },
+      },
+    },
+  },
+}
+
+export default orderIdentity
