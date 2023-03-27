6a7df45c840a1cb92be611a17073ba7fcfdbda57
techstackmedia1 <osagienoah04@gmail.com>
2023-03-27 06:07:34 -0700

    feat: include loading animation in a loading state (MostSearched.tsx)

--- src/components/MostSearched/MostSearched.tsx
+++ src/components/MostSearched/MostSearched.tsx
@@ -20,7 +20,62 @@ const MostSearched = (): JSX.Element => {
 
   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   if (error) {
-    return <div>Error occurred</div>
+    return (
+      <Card className="searchProducts">
+        <div className="title">Most Searched Products</div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          An Error Occured
+        </div>
+      </Card>
+    )
+  }
+
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (!products) {
+    return (
+      <Card className="searchProducts">
+        <style>{`
+          .loader {
+            width: 10px;
+            height: calc(100% - 57px);
+            background-color: #f0f0f0;
+            animation: loading 1s ease-in-out infinite;
+          }
+          
+          @keyframes loading {
+            0% {
+              transform: translateX(0);
+              position: absolute;
+              left: 0;
+              top: 55px;
+            }
+            // 50% {
+            //   transform: translateX(100%);
+            // }
+            100% {
+              transform: translateX(100%);
+              position: absolute;
+              left: 100%;
+              top: 55px;
+            }
+          }
+          
+          `}</style>
+        <div className="title">Most Searched Products</div>
+        <div
+          className="loader"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}></div>
+      </Card>
+    )
   }
 
   const productList = products?.results?.map(
