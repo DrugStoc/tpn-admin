d17bf04b5093f7547003945bea3d561e3d19e958
techstackmedia1 <osagienoah04@gmail.com>
2023-03-31 05:08:19 -0700

    style: style add merchant identity chart component

--- src/config/addMerchant.ts
+++ src/config/addMerchant.ts
@@ -5,11 +5,16 @@ const addMerchant = {
     fontFamily: 'fontFamily',
     ...sections,
   },
+  '.addMerchant .addMerchant-merchant-detail': {
+    width: '61%',
+  },
   '.addMerchant .card': { marginBottom: '50px' },
   '.addMerchant .addMerchant-section': {
     width: '90%',
     top: 0,
     pos: 'absolute',
+    display: 'flex',
+    justifyContent: 'space-between',
   },
   '.addMerchant .addMerchant-section .cardBody': {
     paddingBlock: '40px',
@@ -32,11 +37,11 @@ const addMerchant = {
     gap: '8px',
     cursor: 'pointer',
   },
-  '.addMerchant .addMerchant-section .cardBody form,\n.addMerchant .addMerchant-section .cardBody .deliveryForm':
+  '.addMerchant .addMerchant-section .cardBody form, .addMerchant .addMerchant-section .cardBody .deliveryForm':
     {
       marginTop: '60px',
     },
-  '.addMerchant .addMerchant-section .cardBody form .form,\n.addMerchant .addMerchant-section .cardBody .deliveryForm .form':
+  '.addMerchant .addMerchant-section .cardBody form .form, .addMerchant .addMerchant-section .cardBody .deliveryForm .form':
     {
       display: 'flex',
       justifyContent: 'space-between',
@@ -44,25 +49,68 @@ const addMerchant = {
       marginTop: '-20px',
       marginBottom: '-20px',
     },
-  '.addMerchant .addMerchant-section .cardBody form :where(.inputLabel),\n.addMerchant .addMerchant-section .cardBody .deliveryForm :where(.inputLabel)':
+  '.formInput': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  '.addMerchantInput': {
+    width: '100%',
+  },
+  '.addMerchantIdentity': {
+    w: '34%',
+  },
+  '.addMerchantDetail': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '25px',
+  },
+  '.addMerchantDetail h2': {
+    fontSize: '14px',
+  },
+  '.addMerchantDetail h2 span': {
+    fontSize: '12px',
+    fontWeight: 400,
+  },
+  '.addMerchantDetail ul': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '50%',
+  },
+  '.addMerchantDetail li': {
+    listStyleType: 'none',
+    fontSize: '12px',
+    color: '#AEAAAE',
+  },
+  '.addMerchantDetailChart': {
+    height: '324.6px',
+  },
+  '.addMerchantDetailChartLiner': {
+    borderLeft: '1px solid #939094',
+    marginLeft: '25px',
+    height: '300px',
+  },
+  '.addMerchant .addMerchant-section .cardBody form :where(.inputLabel), .addMerchant .addMerchant-section .cardBody .deliveryForm :where(.inputLabel)':
     {
       marginBottom: '30px',
-      minWidth: '405.922px',
+      // minWidth: '405.922px',
       position: 'relative',
     },
-  '.addMerchant .addMerchant-section .cardBody form :where(.inputLabel) :where(img),\n.addMerchant .addMerchant-section .cardBody .deliveryForm :where(.inputLabel) :where(img)':
+  '.addMerchant .addMerchant-section .cardBody form :where(.inputLabel) :where(img), .addMerchant .addMerchant-section .cardBody .deliveryForm :where(.inputLabel) :where(img)':
     {
       position: 'absolute',
       top: '53px',
       left: '22px',
     },
-  '.addMerchant .addMerchant-section .cardBody form :where(.inputLabel) label,\n.addMerchant .addMerchant-section .cardBody .deliveryForm :where(.inputLabel) label':
+  '.addMerchant .addMerchant-section .cardBody form :where(.inputLabel) label, .addMerchant .addMerchant-section .cardBody .deliveryForm :where(.inputLabel) label':
     {
       fontSize: '14px',
       color: '#1f1c19',
       fontWeight: 500,
+      display: 'block',
     },
-  '.addMerchant .addMerchant-section .cardBody form :where(.inputLabel) input,\n.addMerchant .addMerchant-section .cardBody .deliveryForm :where(.inputLabel) input':
+  '.addMerchant .addMerchant-section .cardBody form :where(.inputLabel) input, .addMerchant .addMerchant-section .cardBody .deliveryForm :where(.inputLabel) input':
     {
       background: '#f9f9fc',
       borderRadius: '4px',
