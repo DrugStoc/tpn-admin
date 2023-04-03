14b0ca1ece561112716bd995c5aed866183df657
techstackmedia1 <osagienoah04@gmail.com>
2023-04-03 07:34:09 -0700

    build: type declaration for voucher history table (VoucherHistoryTable.ts)

---
+++ src/components/VoucherHistory/VoucherHistoryTableData.ts
@@ -0,0 +1,73 @@
+const VoucherHistoryTableData = [
+  {
+    id: 1,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'used' },
+      { 'column 5': 'Wed 3 April, 15:18' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+  {
+    id: 2,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'open' },
+      { 'column 5': 'Wed 3 April, 15:18' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+  {
+    id: 3,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'used' },
+      { 'column 5': 'Wed 3 April, 15:18' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+  {
+    id: 4,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'failed' },
+      { 'column 5': 'Wed 3 April, 15:18' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+  {
+    id: 5,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'used' },
+      { 'column 5': 'Wed 3 April, 15:18' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+  {
+    id: 6,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'open' },
+      { 'column 5': 'Wed 3 April, 15:18' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+]
+
+const VoucherHistoryTableHeadingData = [
+  'empty',
+  'ID',
+  'Name of Product',
+  'Status',
+  'Expiration Date',
+  'Creation Date',
+]
+
+export { VoucherHistoryTableData, VoucherHistoryTableHeadingData }
