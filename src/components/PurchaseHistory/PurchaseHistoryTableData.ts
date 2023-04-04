139f51cc637ebbda2fe0a7fcff0b3392b022f440
techstackmedia1 <osagienoah04@gmail.com>
2023-04-04 07:00:36 -0700

    build: create table data (PurchaseHistoryTableData.ts)

---
+++ src/components/PurchaseHistory/PurchaseHistoryTableData.ts
@@ -0,0 +1,93 @@
+const PurchaseHistoryTableData = [
+  {
+    id: 1,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'Voucher' },
+      { 'column 5': 'Completed' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+  {
+    id: 2,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'Purchased' },
+      { 'column 5': 'Failed' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+  {
+    id: 3,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'Purchased' },
+      { 'column 5': 'Completed' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+  {
+    id: 4,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'Purchased' },
+      { 'column 5': 'Completed' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+  {
+    id: 5,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'Voucher' },
+      { 'column 5': 'Failed' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+  {
+    id: 6,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'Voucher' },
+      { 'column 5': 'Completed' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+  {
+    id: 7,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'Purchased' },
+      { 'column 5': 'Completed' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+  {
+    id: 8,
+    row: [
+      { 'column 2': '#vh0001' },
+      { 'column 3': 'Product Name' },
+      { 'column 4': 'Purchased' },
+      { 'column 5': 'Completed' },
+      { 'column 6': 'Fri 7 Oct, 15:18' },
+    ],
+  },
+]
+
+const PurchaseHistoryTableHeadingData = [
+  'empty',
+  'ID',
+  'Name of Product',
+  'Purchase Nature',
+  'Status',
+  'Date / Time',
+]
+
+export { PurchaseHistoryTableData, PurchaseHistoryTableHeadingData }
