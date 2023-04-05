30039914ab6a47498fdc6c10249f92284237c24f
techstackmedia1 <osagienoah04@gmail.com>
2023-04-05 03:41:32 -0700

    feat: suspense and delete items to customer details navbar toggle suspense button on and off

--- src/components/CustomerDetails/CustomerDetailNav.tsx
+++ src/components/CustomerDetails/CustomerDetailNav.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../shared/Card'
 
 const CustomerDetailNav = ({
@@ -6,6 +6,11 @@ const CustomerDetailNav = ({
   handler,
   handlePrint,
 }: any): JSX.Element => {
+  const [isOn, setIsOn] = useState(false)
+
+  const handleClick = (): void => {
+    setIsOn(!isOn)
+  }
   return (
     <div className="customerNav">
       <div className="customerLinks">
@@ -47,6 +52,68 @@ const CustomerDetailNav = ({
           <span>Print File</span>
         </Card>
       )}
+      {text === 'Customer Details' ? (
+        <div
+          style={{
+            gap: 40,
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            fontSize: '14px',
+            position: 'relative',
+            top: -7,
+          }}>
+          <div
+            onClick={handleClick}
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              gap: 8,
+              cursor: 'pointer',
+            }}>
+            {isOn ? (
+              <img
+                src="https://res.cloudinary.com/bizstak/image/upload/v1678576466/toggle-on_yjtyea.svg"
+                width={30}
+                height={30}
+                alt="toggle on button icon"
+              />
+            ) : (
+              <img
+                src="https://res.cloudinary.com/bizstak/image/upload/v1678576482/toggle-off_trg38c.svg"
+                width={30}
+                height={30}
+                alt="toggle off button icon"
+              />
+            )}
+
+            <span>Suspend User</span>
+            <img
+              src="https://res.cloudinary.com/bizstak/image/upload/v1680690573/info_nf6eom.svg"
+              width={13.33}
+              height={13.33}
+            />
+          </div>
+          <div
+            style={{
+              backgroundColor: '#fff',
+              borderRadius: 4,
+              display: 'flex',
+              alignItems: 'center',
+              padding: '5px 20px',
+              gap: 8,
+            }}>
+            <img
+              src="https://res.cloudinary.com/bizstak/image/upload/v1678790407/delete_pya0qg.svg"
+              width={20}
+              height={20}
+              alt="delete icon"
+            />
+            <span>Delete Customer</span>
+          </div>
+        </div>
+      ) : null}
     </div>
   )
 }
