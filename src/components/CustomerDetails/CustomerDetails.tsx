729dafe4ce29ab1f4cdd48d21587c04e1af7c890
techstackmedia1 <osagienoah04@gmail.com>
2023-04-06 03:30:57 -0700

    feat: 'CustomerDetailNav' => 'NavbarSub', pass list itesms as props to component

--- src/components/CustomerDetails/CustomerDetails.tsx
+++ src/components/CustomerDetails/CustomerDetails.tsx
@@ -3,37 +3,47 @@ import Navbar from '../Navbar/Navbar'
 import Motion from '../shared/Motion'
 import CustomerDetailMain from './CustomerDetailMain'
 import VoucherHistory from '../VoucherHistory/VoucherHistory'
-import CustomerDetailNav from './CustomerDetailNav'
 import PurchaseHistory from '../PurchaseHistory/PurchaseHistory'
+import NavbarSub from '../Navbar/NavbarSub'
 const CustomersDetails = ({ arrow }: any): JSX.Element => {
   const [text, setText] = useState('Customer Details')
   const handler = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>): void => {
     setText((e.target as HTMLSpanElement).innerText)
   }
 
-  const tableRef = useRef<HTMLDivElement>(null);
+  const tableRef = useRef<HTMLDivElement>(null)
   const handlePrint = (): void => {
     if (tableRef.current !== null) {
-      const printContents = tableRef.current.innerHTML;
-      const newWindow = window.open('', '_blank');
-      newWindow?.document.write(`<html><head><title>Print ${text} | DrugStoc TPN Admin</title>`);
-      newWindow?.document.write('<style>');
-      newWindow?.document.write('table { border-collapse: collapse; width: 100%; font-family: sans-serif; }');
-      newWindow?.document.write('th, td { text-align: left; padding: 8px; }');
-      newWindow?.document.write('th { background-color: #f2f2f2; }');
-      newWindow?.document.write('tr:hover { background-color: #f5f5f5; }');
-      newWindow?.document.write('h1 { font-size: 24px; margin-top: 0; margin-bottom: 16px; }');
-      newWindow?.document.write('p { font-size: 16px; margin-top: 0; margin-bottom: 16px; }');
-      newWindow?.document.write('</style></head><body>');
-      newWindow?.document.write(`<div style="margin: 16px 0;"><h1>Customer Details: ${text}</h1><p>Here are the details of the customer's ${text.toLowerCase()}:</p></div>`);
-      newWindow?.document.write(printContents);
-      newWindow?.document.write('</body></html>');
-      newWindow?.document.close();
-      newWindow?.focus();
-      newWindow?.print();
+      const printContents = tableRef.current.innerHTML
+      const newWindow = window.open('', '_blank')
+      newWindow?.document.write(
+        `<html><head><title>Print ${text} | DrugStoc TPN Admin</title>`
+      )
+      newWindow?.document.write('<style>')
+      newWindow?.document.write(
+        'table { border-collapse: collapse; width: 100%; font-family: sans-serif; }'
+      )
+      newWindow?.document.write('th, td { text-align: left; padding: 8px; }')
+      newWindow?.document.write('th { background-color: #f2f2f2; }')
+      newWindow?.document.write('tr:hover { background-color: #f5f5f5; }')
+      newWindow?.document.write(
+        'h1 { font-size: 24px; margin-top: 0; margin-bottom: 16px; }'
+      )
+      newWindow?.document.write(
+        'p { font-size: 16px; margin-top: 0; margin-bottom: 16px; }'
+      )
+      newWindow?.document.write('</style></head><body>')
+      newWindow?.document.write(
+        `<div style="margin: 16px 0;"><h1>Customer Details: ${text}</h1><p>Here are the details of the customer's ${text.toLowerCase()}:</p></div>`
+      )
+      newWindow?.document.write(printContents)
+      newWindow?.document.write('</body></html>')
+      newWindow?.document.close()
+      newWindow?.focus()
+      newWindow?.print()
     }
-  };
-  
+  }
+
   return (
     <Motion>
       <div className="addCustomer">
@@ -51,17 +61,32 @@ const CustomersDetails = ({ arrow }: any): JSX.Element => {
           }
         />
         <section className="addCustomer-section">
-          <CustomerDetailNav handlePrint={handlePrint} text={text} handler={handler} />
+          <NavbarSub
+            firstItem="Customer Details"
+            secondItem="Voucher History"
+            thirdItem="Purchase History"
+            forthItem="Suspend User"
+            fifthItem="Delete Customer"
+            handlePrint={handlePrint}
+            text={text}
+            handler={handler}
+          />
           {text === 'Customer Details' ? (
-            <div ref={tableRef} className='print-table'>
+            <div ref={tableRef} className="print-table">
               <CustomerDetailMain />
-              </div>
+            </div>
           ) : text === 'Voucher History' ? (
-            <div ref={tableRef} style={{ position: 'relative', top: -155 }} className='print-table'>
+            <div
+              ref={tableRef}
+              style={{ position: 'relative', top: -155 }}
+              className="print-table">
               <VoucherHistory />
             </div>
           ) : (
-            <div ref={tableRef} style={{ position: 'relative', top: -155 }} className='print-table'>
+            <div
+              ref={tableRef}
+              style={{ position: 'relative', top: -155 }}
+              className="print-table">
               <PurchaseHistory />
             </div>
           )}
