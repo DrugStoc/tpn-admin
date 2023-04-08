57936a4894ac678793a0e163c84eb4f2084dac2d
techstackmedia1 <osagienoah04@gmail.com>
2023-04-08 09:15:49 -0700

    style: add an overflow-hidden prop to css rule

--- src/components/Navbar/NavbarSub.tsx
+++ src/components/Navbar/NavbarSub.tsx
@@ -41,6 +41,7 @@ const NavbarSub = ({
                     box-shadow: 0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12);
                     border-radius: 4px;
                     width: 100%;
+                    overflow: hidden;
                 }
                 .detailMenu li:first-of-type:hover {
                     border-top-right-radius: 4px;
