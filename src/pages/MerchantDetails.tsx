295248ed10ad10c1b77f63890564c43d9e862359
techstackmedia1 <osagienoah04@gmail.com>
2023-04-12 06:27:46 -0700

    feat: include merchant details page (MerchantDetails.tsx)

---
+++ src/pages/MerchantDetails.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MerchantDetailsAside from '../components/Merchants/MerchantDetails'
+import SidebarAside from '../components/Sidebar/Sidebar'
+
+const MerchantDetails = (): JSX.Element => {
+  return (
+    <>
+      <SidebarAside />
+      <MerchantDetailsAside />
+    </>
+  )
+}
+
+export default MerchantDetails
