260c6558ba42090dc4bf626cd4c493b1a28ea414
techstackmedia1 <osagienoah04@gmail.com>
2023-04-28 16:14:25 -0700

    feat: adding a fallback route while page is on load (on page refresh and login check)

--- src/utils/PrivateRoutes.tsx
+++ src/utils/PrivateRoutes.tsx
@@ -1,9 +1,22 @@
 import { Outlet, Navigate } from 'react-router-dom'
 import LoginContext from '../context/LoginContext'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 const PrivateRoutes = (): JSX.Element => {
   const { loggedIn } = useContext(LoginContext)
+  
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const checkLoggedIn = async (): Promise<void> => {
+      setLoading(false)
+    }
+    void checkLoggedIn()
+  }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
   return loggedIn === true ? <Outlet /> : <Navigate to="login" />
 }
 
