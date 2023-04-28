99c9ff5c86c441e3fea2afec7847196b76365138
techstackmedia1 <osagienoah04@gmail.com>
2023-04-28 16:04:56 -0700

    feat: add product detail page within the context of the root route

---
+++ src/pages/ProductDetails.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductDetailsAside from '../components/Products/ProductDetails'
+import SidebarAside from '../components/Sidebar/Sidebar'
+
+const ProductDetails = (): JSX.Element => {
+  return (
+    <>
+      <SidebarAside />
+      <ProductDetailsAside />
+    </>
+  )
+}
+
+export default ProductDetails
